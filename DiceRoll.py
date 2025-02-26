e8af7495374a8f77f30f4e0ec2f1c72e824261f2
adamthies17 <adamthies17@gmail.com>
2025-02-26 16:08:33 +0000

    done with lab

--- DiceRoll.py
+++ DiceRoll.py
@@ -6,13 +6,27 @@ import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
+  numRolls = 10000000
+  for count in range(numRolls):
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    total = dice1 + dice2
+
+    rolls[total -2] = rolls[total -2] + 1
+
   
+
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+  print(rolls)
+  num = 2
+  for r in rolls:
+    percent = round(r / numRolls * 100, 1)
+    print(num , ":", r, percent)
+    num = num + 1
 
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -4,11 +4,28 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   for line in textFile:
-    print(line)
+    lineCount += 1
+    words = line.split()
+    
+
+    for word in words:
+      wordCount += 1
+      
+
+      for letter in word:
+        letterCount += 1
+        
+
   
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Letter:", letterCount)
 
 if __name__ == '__main__':
   main()
